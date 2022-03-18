de8654db78885ed7e904e87d00d826b4b4be2201
Guillaume <guillaume.ganneval@etud.u-picardie.fr>
2022-03-18 17:30:54 +0100

    Fin logique
    Logique complété, reste quelques exceptions que je n'arrive pas à gérer.

--- app/src/main/java/com/example/mentalcounting/activities/GameActivity.java
+++ app/src/main/java/com/example/mentalcounting/activities/GameActivity.java
@@ -2,22 +2,69 @@ package com.example.mentalcounting.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mentalcounting.R;
+import com.example.mentalcounting.models.OperationModel;
+import com.example.mentalcounting.models.exceptions.OperatorException;
+import com.example.mentalcounting.services.OperationService;
+import com.example.mentalcounting.services.ResolutionService;
 
 public class GameActivity extends AppCompatActivity {
 
+    private EditText et;
+    private OperationModel om;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        et = findViewById(R.id.Reponse);
+        TextView tw = findViewById(R.id.Operation);
+
+        OperationService os = new OperationService();
+        om = os.operation();
+
+        String operation = om.getNumber1() + "   " + om.getOperator() + "   " + om.getNumber2();
+        tw.setText(operation);
+
+        Button SubmitButton = findViewById(R.id.Valider);
+        Button ResetButton = findViewById(R.id.ResetButton);
+
+        SubmitButton.setOnClickListener(view -> {
+            try {
+                Submit();
+            } catch(OperatorException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+
+
+        ResetButton.setOnClickListener(view -> {
+            try {
+                Reset();
+            } catch (OperatorException e) {
+                e.printStackTrace();
+            }
+        });
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -25,6 +72,7 @@ public class GameActivity extends AppCompatActivity {
         inflater.inflate(R.menu.gamemenu, menu);
 
         return true;
+
     }
 
     @Override
@@ -39,4 +87,35 @@ public class GameActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    private void Submit() throws OperatorException {
+        int result = Integer.parseInt(et.getText().toString());
+        verification(result);
+    }
+
+    private void Reset() throws OperatorException{
+        finish();
+        startActivity(getIntent());
+    }
+
+
+
+
+    @SuppressLint("ResourceAsColor")
+    private void verification(int result) throws OperatorException {
+        ResolutionService vs = new ResolutionService();
+
+        TextView resultTextView = findViewById(R.id.Result);
+        if(vs.Verif(om, result)){
+
+            resultTextView.setTextColor(R.color.green);
+            resultTextView.setText(R.string.GoodAnswer);
+
+
+        } else {
+            resultTextView.setTextColor(R.color.red);
+            resultTextView.setText(R.string.WrongAnswer);
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/mentalcounting/services/OperationService.java
+++ app/src/main/java/com/example/mentalcounting/services/OperationService.java
@@ -7,30 +7,23 @@ public class OperationService {
 
     public OperationModel operation(){
         String operators = genOperator();
-        if(operators=="/"){
-            int div= genDiv();
-            return new OperationModel(genDiv()*div,div,genOperator());
-        }else{
-            return new OperationModel(genNb(),genNb(),genOperator());
-        }
+
+        return new OperationModel(genNb(),genNb(),genOperator());
+
 
 
     }
 
-    public int genDiv(){
+
+
+    public int genNb(){
         int max = 99;
         Random random = new Random();
         return random.nextInt(max)+1;
     }
 
-    public int genNb(){
-        int max = 999;
-        Random random = new Random();
-        return random.nextInt(max)+1;
-    }
-
     public String genOperator(){
-        String[] operators = {"+","-","*","/"};
+        String[] operators = {"+","-","*"};
         int random = new Random().nextInt(operators.length);
         return operators[random];
     }
